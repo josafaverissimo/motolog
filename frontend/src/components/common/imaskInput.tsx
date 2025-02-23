95f4f4b73f8b28404db2b07754ad69891a1ed20a
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2025-02-23 19:12:17 -0300

    fix: pass inputRef to imask inputs

--- frontend/src/components/common/imaskInput.tsx
+++ frontend/src/components/common/imaskInput.tsx
@@ -3,5 +3,5 @@ import type { Ref } from "react";
 import { IMaskMixin } from "react-imask";
 
 export const IMaskInput = IMaskMixin(({ inputRef, ...props }) => (
-	<Input {...props} ref={inputRef as Ref<HTMLInputElement>} />
+	<Input ref={inputRef as Ref<HTMLInputElement>} {...props} />
 ));
